f06ed8baf1ab3124e1d771afb2262ff129de6529
Justin DuPont <justindpnt@gmail.com>
2022-02-17 17:38:57 -0600

    Fixed overlapping sound
    When a collision happens and both objects have a sound to play on collision, only the faster moving object will play a sound

--- Assets/Scripts/Items/SoundableItem.cs
+++ Assets/Scripts/Items/SoundableItem.cs
@@ -6,27 +6,56 @@ public class SoundableItem : MonoBehaviour
 {
     //AudioSource soundEffect;
     //bool hasPlayed = false;
-    AudioSource[] collisionSounds; 
+    AudioSource[] collisionSounds;
+    Rigidbody objectRB;
 
     private void Awake()
     {
+        objectRB = GetComponent<Rigidbody>();
+
+        //If this object has an audio source, then we put that 
         if (GetComponent<AudioSource>())
         {
             collisionSounds = GetComponents<AudioSource>();
-            //soundEffect = GetComponent<AudioSource>();
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        bool playSound = false;
+
         if (collision.relativeVelocity.magnitude > 2f)
         {
-            int indexOfSoundToPlay = Random.Range(0, collisionSounds.Length);
+            //if the object has a parent (which means it is a collider object)
+            if (collision.collider.transform.parent != null)
+            {
+                //if that object has a soundable item
+                if (collision.collider.transform.parent.gameObject.GetComponent<SoundableItem>())
+                {
+                    //Only the faster moving object in a collsiion should make a sound
+                    if (objectRB.velocity.magnitude > collision.collider.transform.parent.gameObject.GetComponent<Rigidbody>().velocity.magnitude)
+                    {
+                        playSound = true;
+                    }
+                }
+                else
+                {
+                    playSound = true;
+                }
+            }
+            else
+            {
+                playSound = true;
+            }
 
-            //The sound of a collision is depedent on the speed of the collision
-            collisionSounds[indexOfSoundToPlay].volume = Mathf.Clamp01(collision.relativeVelocity.magnitude / 40); 
-            collisionSounds[indexOfSoundToPlay].Play();
+            if (playSound)
+            {
+                int indexOfSoundToPlay = Random.Range(0, collisionSounds.Length);
+
+                //The sound of a collision is depedent on the speed of the collision
+                collisionSounds[indexOfSoundToPlay].volume = Mathf.Clamp01(collision.relativeVelocity.magnitude / 40);
+                collisionSounds[indexOfSoundToPlay].Play();
+            }
         }
     }
-
 }
